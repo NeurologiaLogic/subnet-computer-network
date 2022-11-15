ef38c86daad4860e073f677265c8abf0d43e1279
Neurologia <patrickkwon777@gmail.com>
2022-11-15 19:06:15 +0700

    fixing bugs in BA

--- subnetting.js
+++ subnetting.js
@@ -1,7 +1,7 @@
-let searches = [
-  "192.168.0.64/27",
-  "172.16.5.253/29",
-]
+let searches =
+"172.16.64.64/27\
+".split(" ").filter(val=>val!=="")
+
 
 const intToDecimal = (val) => {
   val = parseInt(val)
@@ -91,6 +91,7 @@ const printDots = (val) => {
 }
 for (let search of searches) {
   const [ip, subnet] = search.split('/')
+  automationValidation.push()
   console.log("==================================================================")
   console.log(`IP: ${decimalToIp(printDots(ipToDecimal(ip)))}`)
   console.log(`IP in Decimal: ${printDots(ipToDecimal(ip))}`)
@@ -103,15 +104,15 @@ for (let search of searches) {
 }
 //diatas untuk mengvalidasi NA sebuah network
 
-const NA = "172.16.0.0/16"
+const NA = "172.16.128.0/18"
 const names =
-  "IBOX \
-  Beehive \
-  Starbucks \
-  BCA \
-  LKC \
-  ATM_BCA \
-  SRSC \
+  "IBOX\
+  Beehive\
+  Starbucks\
+  BCA\
+  LKC\
+  ATM_BCA\
+  SRSC\
   Admisi"
   .split(" ")
   .filter(str => str !== "")
@@ -127,15 +128,15 @@ const sizes =
   .split(" ")
   .filter(str => str !== "")
   .map(val => parseInt(val))
-console.log(names,sizes)
-if(names.length!==sizes.length){
+console.log(names, sizes)
+if (names.length !== sizes.length) {
   console.log("Error: names and sizes must have same length")
   process.exit(1)
 }
 let gedung = []
-names.map((name,index)=>{
+names.map((name, index) => {
   let temp = {}
-  temp[name]=sizes[index]
+  temp[name] = sizes[index]
   return gedung.push(temp)
 })
 gedung.sort((a, b) => Object.values(b)[0] - Object.values(a)[0])
@@ -143,17 +144,19 @@ gedung.sort((a, b) => Object.values(b)[0] - Object.values(a)[0])
 //2. cari size k yang lebih besar dari 2^k>size+2
 //3. print subnetting yang dimiliki
 //4. either sort as a long list or sort per lantai
-
 const getK = (size) => {
   let k = 0
-  while (2 ** k < size + 2) {
+  while (size + 2 >= 2**k) {
     ++k
   }
-  return k
+  return k;
 }
+
 const subnetFromK = (k) => {
   let subnet = 32 - k;
-  console.log(`Subnet mask: ${decimalToIp(printDots(subnetToDecimal(subnet)))}`)
+  let decimalSubnet = decimalToIp(printDots(subnetToDecimal(subnet)))
+  console.log(`Subnet mask: ${decimalSubnet}`)
+  return decimalSubnet;
 }
 
 const getIP = (prev_ip, k) => {
@@ -176,24 +179,25 @@ const getIP = (prev_ip, k) => {
   ip = ip.map(val => val.toString())
   return ip.join(".")
 }
-const getRangeIP = (ipRange,k) =>{
+const getRangeIP = (ipRange, k) => {
   let ip = ipRange.split('.').map(val => parseInt(val))
   let counterKtoFulfill = k;
-  if(counterKtoFulfill==-1){
-    //decrement the ip
-    if (ip[3] === 0) {
-      ip[3] = 255
-      if (ip[2] === 0) {
-        ip[2] = 255
-        if (ip[1] === 0) {
-          ip[1] = 255
-          ip[0] -= 1
-        } else ip[1] -= 1
-      } else ip[2] -= 1
+  if (counterKtoFulfill < 0) {
+    while (counterKtoFulfill < 0) {
+      if (ip[3] === 0) {
+        ip[3] = 255
+        if (ip[2] === 0) {
+          ip[2] = 255
+          if (ip[1] === 0) {
+            ip[1] = 255
+            ip[0] -= 1
+          } else ip[1] -= 1
+        } else ip[2] -= 1
+      } else ip[3] -= 1
+      counterKtoFulfill++;
     }
-    else ip[3] -= 1
-  }
-  else{
+  } else {
+    while (counterKtoFulfill) {
       if (ip[3] === 255) {
         ip[3] = 0
         if (ip[2] === 255) {
@@ -205,30 +209,45 @@ const getRangeIP = (ipRange,k) =>{
         } else ip[2] += 1
       } else ip[3] += 1
       counterKtoFulfill--;
+    }
   }
 
   ip = ip.map(val => val.toString())
   return ip.join(".")
 }
 let firstIP = NA.split('/')
-Object.entries(gedung).forEach(([key, value]) => {
+const fs = require('fs')
+const appendData = (...str) => {
+  for (const s of str) {
+    fs.appendFile('./data.csv', s, err => {
+      if (err) {
+        console.error(err)
+        return
+      }
+    })
+  }
+}
+appendData("Name,Number of Electronics,NA,Network Range,BA,Subnet Mask\n");
+appendData("==============================================================\n");
+Object.entries(gedung).forEach(([_, value]) => {
   console.log("==================================================================")
   console.log(`Gedung ${Object.keys(value)}: ${ Object.values(value)[0]}`)
   let k = getK(Object.values(value)[0])
-  subnet=32-k;
-  let localNA =null
+  let cidr = 32 - k;
+  let localNA = null
   let range_below = null;
   let range_top = null;
   let localBA = null
   localNA = `${firstIP[0]}`;
-  range_below = getRangeIP(firstIP[0],1)
+  range_below = getRangeIP(firstIP[0], 1)
   localBA = `${getIP(firstIP[0],k)}/${firstIP[1]}`.split("/")
-  range_top = getRangeIP(localBA[0],-1)
-
-  console.log(`NA: ${localNA}/${subnet}`)
-  console.log(`Range IP: ${range_below}/${subnet} - ${range_top}/${subnet}`)
-  console.log(`BA: ${localBA[0]}/${subnet}`)
-  subnetFromK(k)
+  range_top = getRangeIP(localBA[0], -2)
+  console.log(`NA: ${localNA}/${cidr}`)
+  console.log(`Range IP: ${range_below}/${cidr} - ${range_top}/${cidr}`)
+  console.log(`BA: ${getRangeIP(localBA[0], -1)}/${cidr}`)
+  let decimalSubnet = subnetFromK(k)
+  // console.log(`${Object.keys(value)},`, `${Object.values(value)},`, `${localNA}/${cidr},`, `${range_below}/${cidr} - ${range_top}/${cidr},`, `${getRangeIP(localBA[0], -1)}/${cidr},`, `${decimalSubnet}\n`)
+  appendData(`${Object.keys(value)},`, `${Object.values(value)},`, `${localNA}/${cidr},`, `${range_below}/${cidr} - ${range_top}/${cidr},`, `${getRangeIP(localBA[0], -1)}/${cidr},`, `${decimalSubnet}\n`)
   firstIP = localBA
-  console.log(" ")
+  console.log("")
 })
